aa726da4ff9998c70ea492e0c8c1a5eee5979da7
Spellmell <spellmell@protonmail.com>
2021-12-21 08:28:29 -0300

    Commit kata_12 update for a more efficient method

--- 12_is_a_number_prime.py
+++ 12_is_a_number_prime.py
@@ -23,10 +23,8 @@ is_prime(-1) /* false */
 
 """
 
-# import random as rnd
-#
-# matrix = []
-#
+# my first attempt:
+# import random as rnd#
 #
 # def is_prime(num):
 #     matrix = [i for i in range(2, num+1)]
@@ -45,18 +43,54 @@ is_prime(-1) /* false */
 #     else:
 #         return False
 #
-#
 # print(f"\n{is_prime(rnd.randint(0,666))}")
 
+# my first attempt 2:
+# method: eratosthenes sieve
+# functional but does not pass the efficiency in attempt for very high numbers:
+# "Execution Timed Out (12000 ms)"
+# 2000000000 qhdp!
+#
+# from random import randint as rnd
+# def is_prime(num):
+#     m = [i for i in range(2, num+1)]
+#     [[m.remove(k) for k in m if k % j == 0 and k != j]
+#      for j in m if j ** 2 <= num]
+#     print(num)
+#     return True if num in m else False
+# print(is_prime(rnd(0, 666)))
 
-import random as rnd
+# my second attempt:
+# is also inefficient in the attempt:
+# from random import randint as rnd
+# def is_prime(num): return [False if num <= 1 or num > 2 and num % 2 == 0 else all(
+#             num % i for i in range(2, num))][0]
+# rndn = rnd(0, 666)
+# print(f"Is {rndn} prime? {is_prime(rndn)}")
+
+# https://docs.codewars.com/training/troubleshooting/
+# "There is a known issue with Codewars' runner for Python, which makes random
+# tests much slower than they used to be some time ago. Read through the kata
+# discourse and see if anyone mentions some problems with timeouts for Python."
+
+# from math import factorial as f
+# def is_prime(num): return f(num-1) % num == num - 1
+# print(is_prime(153))
+
+# http://montreal.pm.org/tech/neil_kandalgaonkar.shtml
+# import re
+# def is_prime(n): return not re.match(r'^1?$|^(11+?)\1+$', n*'1')
+# print(is_prime(153))
 
 
-def is_prime(n):
-    m = [i for i in range(2, n+1)]
-    [[m.remove(k) for k in m if k % j == 0 and k != j]
-     for j in m if j ** 2 <= n]
-    return True if n in m else False
+from random import randint as rnd
+from math import sqrt as q
 
 
-print(is_prime(rnd.randint(0, 666)))
+def is_prime(num):
+    print(num)
+    return [False if num <= 1 or num > 2 and num % 2 == 0 else all(
+            num % i for i in range(2, round(q(num)+1)))][0]
+
+
+print(is_prime(rnd(0, 153)))
